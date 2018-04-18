48a96e6982c6bdeb9539eb68b67ccd207c809aab
Francisco M <franji2@gmail.com>
2018-04-18 17:03:03 +0200

    add clickable link

--- src/components/pages/devices/flyouts/SIMManagement/SIMManagement.js
+++ src/components/pages/devices/flyouts/SIMManagement/SIMManagement.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 import React, { Component } from 'react';
+import { Trans } from 'react-i18next';
+import { Link } from 'react-router-dom'
 
 import {
   Flyout,
@@ -26,7 +28,11 @@ export class SIMManagement extends Component {
         <FlyoutContent>
           <div className="sim-management-container">
             <div className="sim-management-header">{t('devices.flyouts.SIMManagement.header')}</div>
-            <div className="sim-management-descr">{t('devices.flyouts.SIMManagement.description')}</div>
+            <div className="sim-management-descr">
+              <Trans i18nKey="devices.flyouts.SIMManagement.description">
+                Feature is... <Link to={t('devices.flyouts.SIMManagement.url')} target="_blank">{t('devices.flyouts.SIMManagement.url')}</Link> ...your account.
+              </Trans>
+            </div>
           </div>
         </FlyoutContent>
       </Flyout>
